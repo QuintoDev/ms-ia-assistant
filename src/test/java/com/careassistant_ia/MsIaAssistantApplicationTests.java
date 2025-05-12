5efe62bb7785f3ac7d477ec4d895c01aff0a667f
quinto.dev <fhqa3d@gmail.com>
2025-05-11 22:24:01 -0500

    add: se sube repo base

---
+++ src/test/java/com/careassistant_ia/MsIaAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careassistant_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIaAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
